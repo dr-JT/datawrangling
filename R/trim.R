1a0e5303ac9c95e38305a2d9403370e8767e2ade
Jason Tsukahara <tsuky19@gmail.com>
2018-08-03 10:44:02 -0400

    create trimming function

---
+++ R/trim.R
@@ -0,0 +1,21 @@
+#' A Data Transformation Function
+#'
+#' This function will trim a variable(s) based on a zscore cutoff
+#' @param x dataframe
+#' @param variables variables c() to be trimmed
+#' @param cutoff zscore cutoff to use for trimming (default: 3.5)
+#' @keywords trim
+#' @export
+#' @examples
+#' trim(x, variables = c(), cutoff = 3.5)
+
+trim <- function(x, variables = c(), cutoff = 3.5){
+  x <- zscore(x, subset = variables)
+  for (i in variables){
+    zscored <- paste(i, "_z", sep = "")
+    x <- dplyr::mutate(x, filtered = ifelse(get(zscored)>cutoff,NA,ifelse(get(zscored)<(cutoff*-1),NA,get(i))))
+    x[i] <- x["filtered"]
+    x <- dplyr::select(x, -get(zscored))
+  }
+  x <- dplyr::select(x, -filtered)
+}
