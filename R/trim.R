50e2a0f3941384ca90ac47d31b337f4cfd96005f
dr-JT <tsuky19@gmail.com>
2018-08-08 19:18:54 -0400

    change how column is replaced

--- R/trim.R
+++ R/trim.R
@@ -4,18 +4,18 @@
 #' @param x dataframe
 #' @param variables variables c() to be trimmed
 #' @param cutoff zscore cutoff to use for trimming (default: 3.5)
+#' @param context Name of column to group by
 #' @keywords trim
 #' @export
 #' @examples
 #' trim(x, variables = c(), cutoff = 3.5)
 
-trim <- function(x, variables = c(), cutoff = 3.5){
-  x <- center(x, variables = variables, standardized = TRUE)
+trim <- function(x, variables = c(), cutoff = 3.5, context = ""){
+  x <- center(x, variables = variables, standardized = TRUE, context = context)
   for (i in variables){
     zscored <- paste(i, "_z", sep = "")
     x <- dplyr::mutate(x, filter = ifelse(get(zscored)>cutoff,NA,ifelse(get(zscored)<(cutoff*-1),NA,get(i))))
-    x[i] <- x["filter"]
-    x <- dplyr::select(x, -(zscored))
+    x <- dplyr::select(x, -(zscored), -(i))
+    colnames(x)[which(colnames(x)=="filter")] <- i
   }
-  x <- dplyr::select(x, -filter)
 }
