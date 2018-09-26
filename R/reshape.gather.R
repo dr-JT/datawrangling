4202758b57def05098ca01e1f557446fe7a86305
dr-JT <tsuky19@gmail.com>
2018-09-26 16:11:46 -0400

    update parameteres

--- R/reshape.gather.R
+++ R/reshape.gather.R
@@ -6,16 +6,18 @@
 #' @param variable.name The variable used for spreading
 #' @param values If using more than one variables column then specify name of a new combined column
 #' @param id What column is not being reorganized and needs to be preserved. Usually "Subject"
-#' @param separate.into Passed to spread() fill parameter
 #' @param separate.pattern stuff
 #' @keywords reshape
 #' @export reshape.gather
 #' @examples
 #' reshape.gather(x, variables = "variable", values = c("value1", "value2"), by = "Subject")
 
-reshape.gather <- function(x, variable.name, values, id = NULL, separate.into = NULL, separate.pattern = NULL){
-  if (!is.null(separate.into)){
-    variable.name <- "placeholder"
+reshape.gather <- function(x, variable.names, values, id = NULL, separate.pattern = NULL){
+  if (length(variable.names)>1){
+    separate.into <- variable.names
+    variable.names <- "placeholder"
+  } else {
+    separate.into <- NULL
   }
 
   if (is.null(id)){
@@ -23,21 +25,21 @@ reshape.gather <- function(x, variable.name, values, id = NULL, separate.into = 
     for (i in seq_along(values)){
       y[[i]] <- dplyr::select(x, contains(values[i]))
       columns <- colnames(y[[i]])[which(colnames(y[[i]])!=id)]
-      y <- tidyr::gather(y[[i]], key = variable.name, value = values[i], columns)
+      y <- tidyr::gather(y[[i]], key = variable.names, value = values[i], columns)
     }
-    x <- plyr::join_all(y, by = variable.name)
+    x <- plyr::join_all(y, by = variable.names)
   } else {
     y <- list()
     for (i in seq_along(values)){
       y[[i]] <- dplyr::select(x, id, contains(values[i]))
       columns <- colnames(y[[i]])[which(colnames(y[[i]])!=id)]
-      y <- tidyr::gather(y[[i]], key = variable.name, value = values[i], columns)
+      y <- tidyr::gather(y[[i]], key = variable.names, value = values[i], columns)
     }
-    x <- plyr::join_all(y, by = c(id, variable.name))
+    x <- plyr::join_all(y, by = c(id, variable.names))
   }
 
   if (!is.null(separate.into)){
-    x <- tidyr::separate(x, variable.name, into = separate.into, sep = separate.pattern)
+    x <- tidyr::separate(x, variable.names, into = separate.into, sep = separate.pattern)
   }
   return(x)
 }
