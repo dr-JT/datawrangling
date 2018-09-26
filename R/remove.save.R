0fd2edb5ebfef35e86442098c6a57d36fb9527f9
dr-JT <tsuky19@gmail.com>
2018-09-26 18:54:53 -0400

    simplify remove.save

--- R/remove.save.R
+++ R/remove.save.R
@@ -4,17 +4,16 @@
 #' centered values. There is also the option to center within context.
 #' @param x dataframe
 #' @param remove the dataframe that contains subjects to be removed
-#' @param save folder directory path to save removed data
-#' @param taskname used to name the file
+#' @param output folder directory path to save removed data
 #' @keywords remove
 #' @export
 #' @examples
 #' remove.save(data, remove = data_remove, save = "data/remove", taskname = "Flanker")
 
-remove.save <- function(x, remove = "", save = "", taskname = "") {
+remove.save <- function(x, remove = "", output = "") {
   if (nrow(remove)>0){
     dir.create(save, showWarnings = FALSE)
-    write_delim(remove, path = paste(save, "/", taskname, "_remove.txt", sep = ""), delim = "\t", na = "")
+    write_delim(remove, path = output, delim = "\t", na = "")
     subj.remove <- unique(remove$Subject)
     ## Remove them!
     x <- filter(x, !(Subject %in% subj.remove))
