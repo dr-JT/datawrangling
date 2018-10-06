6a5b2561b1df7902d80fb72c1319f48844894681
Jason Tsukahara <tsuky19@gmail.com>
2018-10-06 12:53:09 -0400

    create standard error function

---
+++ R/se.R
@@ -0,0 +1,17 @@
+#' A Task Scoring Function
+#'
+#' Calculates the standard error of the mean
+#' @param x vector of values to calculate the se
+#' @param na.rm Logical. Remove missing values in calculation?
+#' @keywords se
+#' @export
+#' @examples
+#' se(x)
+
+se <- function(x, na.rm = FALSE){
+  if (na.rm==TRUE){
+    x <- x[which(!is.na(x))]
+  }
+  se <- sd(x)/sqrt(length(x))
+  return(se)
+}
