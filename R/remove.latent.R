bec52e4bd3a702fdb68c912ea192c4ff419f796a
Jason Tsukahara <tsuky19@gmail.com>
2018-11-09 09:31:51 -0500

    change parameter names

--- R/remove.latent.R
+++ R/remove.latent.R
@@ -5,14 +5,14 @@
 #' @param factor.list list of factors and tasks. ex. `list(WMC = c("OSpan", "RotSpan", SymSpan"), Gf = c("RAPM", "NumberSeries", "LetterSets"))`
 #' @param missing.allowed Proportion of tasks allowed to be missing
 #' @param id Subject ID variable
-#' @param removed.dir File directory to save removed subjects to
-#' @param removed.file File name to save removed subjects to
+#' @param output.dir File directory to save removed subjects to
+#' @param output.file File name to save removed subjects to
 #' @keywords remove
 #' @export remove.latent
 #' @examples
 #' # Example
 
-remove.latent <- function(x, factor.list, missing.allowed, id, removed.dir = NULL, removed.file = NULL){
+remove.latent <- function(x, factor.list, missing.allowed, id, output.dir = NULL, output.file = NULL){
   x.remove <- list()
   for (f in seq_along(factor.list)){
     x.remove[[f]] <- dplyr::mutate(x, missing = 0)
@@ -26,11 +26,11 @@ remove.latent <- function(x, factor.list, missing.allowed, id, removed.dir = NUL
   }
   x.remove <- plyr::join_all(x.remove, by = id, type = "full")
 
-  if (is.null(removed.dir)){
+  if (is.null(output.dir)){
     subj.remove <- unique(x.remove$Subject)
     x <- dplyr::filter(x, !(Subject %in% subj.remove))
   } else {
-    x <- remove.save(x, x.remove, output.dir = removed.dir, output.file = removed.file)
+    x <- remove.save(x, x.remove, output.dir = output.dir, output.file = output.file)
   }
   return(x)
 }
