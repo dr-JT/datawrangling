4a2ae460914e336cf2fcedc3d179b8b4ece9bf9b
Jason Tsukahara <tsuky19@gmail.com>
2018-11-13 15:38:51 -0500

    deprecate files.join

--- R/files_join.R
+++ R/files_join.R
@@ -7,21 +7,11 @@
 #' @param na How are missing values defined in files to be merged. Passed to `readr::write_delim()`
 #' @param output.file File name and path to be saved to
 #' @param id Subject ID variable name. Passed onto `plyr::join_all(by = id)`
-#'
-#' @templateVar fun files.join
-#' @template template-depr_fun
-NULL
-
-#' @templateVar old files.join
-#' @templateVar new files_join
-#' @template template-depr_pkg
-#'
 #' @export
 #' @examples
-#' files.join(path = "./Data", pattern = ".txt")
+#' files_join(path = "./Data", pattern = ".txt")
 
-files.join <- function(path = "", pattern = "", delim = "\t", na = "", output.file = "", id = NULL){
-  .Deprecated("files_join")
+files_join <- function(path = "", pattern = "", delim = "\t", na = "", output.file = "", id = NULL){
   filelist <- list.files(path = path, pattern = pattern, full.names = TRUE)
   import <- list()
   for (i in seq_along(filelist)){
