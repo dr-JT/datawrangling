6b734263b287777e50c806a06f75a26d2e6e6f6c
Jason Tsukahara <tsuky19@gmail.com>
2019-01-19 17:29:35 -0500

    add parameter output.delim

--- R/files_join.R
+++ R/files_join.R
@@ -4,6 +4,7 @@
 #' @param path Folder location of files to be merged
 #' @param pattern Pattern to identify files to be merged
 #' @param delim Delimiter used in files. Passed onto `readr::read_delim()`
+#' @param output.delim Delmiter to use when saving data to file (default = ",")
 #' @param na How are missing values defined in files to be merged. Passed to `readr::write_delim()`
 #' @param output.file File name and path to be saved to
 #' @param id Subject ID variable name. Passed onto `plyr::join_all(by = id)`
@@ -11,7 +12,8 @@
 #' @examples
 #' files_join(path = "./Data", pattern = ".txt")
 
-files_join <- function(path = "", pattern = "", delim = "\t", na = "", output.file = "", id = NULL){
+files_join <- function(path = "", pattern = "", delim = ",",
+                       output.delim = ",", na = "", output.file = "", id = NULL){
   filelist <- list.files(path = path, pattern = pattern, full.names = TRUE)
   import <- list()
   for (i in seq_along(filelist)){
