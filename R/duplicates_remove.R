fbcceb5f00891d1335707fff7120c7dcd9822542
dr-JT <tsuky19@gmail.com>
2019-07-01 17:29:43 -0400

    fix

--- R/duplicates_remove.R
+++ R/duplicates_remove.R
@@ -10,21 +10,24 @@
 #' @examples
 #' duplicates_remove(x, subset = c(colnames(x)), context = "Subject")
 
-duplicates_remove <- function(x, timing.variables = "SessionStartDateTimeUtc", subj.variable = "Subject", taskname = "", output.folder = ""){
-  x_duplicated <- x[,c(subj.variable, timing.variables)]
-  x_duplicated <- x_duplicated[!duplicated(x_duplicated),]
+duplicates_remove <- function(x, timing.variables = c("SessionDate", "SessionTime"),
+                              subj.variable = "Subject", taskname = "",
+                              output.folder = ""){
+  x_duplicates <- x[,c(subj.variable, timing.variables)]
+  subj_identical <- x_reduced[which(duplicated(x_duplicates)),]
 
-  duplicated.subj <- x_duplicated[duplicated(x_duplicated[subj.variable]),][[subj.variable]]
-  x_duplicated <- x_duplicated[which(x_duplicated[[subj.variable]] %in% duplicated.subj),]
+  subj_duplicate <- dplyr::distinct(x_duplicates)
+  subj_duplicate <- subj_duplicate[which(duplicated(subj_duplicate[, subj.variable])),]
+
+  subj_list <- c(subj_identical[[subj.variable]], subj_duplicate[[subj.variable]])
+  x_duplicates <- x_duplicates[which(x_duplicates[[subj.variable]] %in% subj_list), ]
+  x <- x[which(!(x[[subj.variable]] %in% subj_list)),]
 
   if (output.folder!=""){
-    if (nrow(x_duplicated)>0){
+    if (nrow(x_duplicates)>0){
       dir.create(output.folder, showWarnings = FALSE)
-      readr::write_csv(x_duplicated, paste(output.folder, "/", taskname, "_duplicates.csv", sep = ""), na = "")
+      readr::write_csv(x_duplicates, paste(output.folder, "/", taskname, "_duplicates.csv", sep = ""), na = "")
     }
   }
-
-  x <- x[which(!(x[[subj.variable]] %in% duplicated.subj)),]
-
   return(x)
 }
