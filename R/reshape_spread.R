c6f8f8ee9400af1cfc0a8c746b42aeefba992f5b
Jason Tsukahara <tsuky19@gmail.com>
2019-09-11 21:15:54 -0400

    add colname.order parameter

--- R/reshape_spread.R
+++ R/reshape_spread.R
@@ -5,17 +5,21 @@
 #' @param x dataframe
 #' @param variables The variable used for spreading
 #'     (The variables/conditions that values are grouped within)
-#' @param variables_combine.name If using more than one variables column then specify name of a new combined column
-#' @param variables_combine.sep if using more than one variables column then specifiy how values should be seperated when combined
 #' @param values A vector of columns that contain the values
-#' @param id What column is not being reorganized and needs to be preserved. Usually "Subject"
+#' @param id What column is not being reorganized and needs to be preserved.
+#'     Usually "Subject"
+#' @param colname.order The variable/value order to use when naming new columns.
+#'     "variable_value" or "value_variable"
+#' @param variables_combine.sep if using more than one variables column then
+#'     specifiy how values should be seperated when combined
 #' @param fill Passed to spread() fill parameter
 #' @export reshape_spread
 #' @examples
 #'
 
-reshape_spread <- function(x, variables = "", variables_combine.sep = "_",
-                           values = c(), id = "", fill = NA){
+reshape_spread <- function(x, variables = "", values = c(), id = "",
+                           colname.order = "variable_value",
+                           variables_combine.sep = "_", fill = NA){
   if (length(variables) > 1) {
     x <- tidyr::unite(x, placeholder, variables, sep = variables_combine.sep)
     variables <- "placeholder"
@@ -27,7 +31,12 @@ reshape_spread <- function(x, variables = "", variables_combine.sep = "_",
       y[[i]] <- dplyr::select(x, variables, values[i])
       y[[i]] <- tidyr::spread(y[[i]],
                               key = variables, value = values[i], fill = fill)
-      colnames(y[[i]]) <- paste(values[i], colnames(y[[i]]), sep = "_")
+      if (colname.order == "variable_value") {
+        colnames(y[[i]]) <- paste(colnames(y[[i]]), values[i], sep = "_")
+      } else {
+        colnames(y[[i]]) <- paste(values[i], colnames(y[[i]]), sep = "_")
+      }
+
     }
     x <- plyr::join_all(y, type = "full")
   } else {
@@ -37,10 +46,18 @@ reshape_spread <- function(x, variables = "", variables_combine.sep = "_",
       y[[i]] <- tidyr::spread(y[[i]],
                               key = variables, value = values[i], fill = fill)
 
-      colnames(y[[i]])[which(colnames(y[[i]])!=id)] <-
-        paste(values[i],
-              colnames(y[[i]])[which(colnames(y[[i]]) != id)],
-              sep = "_")
+      if (colname.order == "variable_value") {
+        colnames(y[[i]])[which(colnames(y[[i]])!=id)] <-
+          paste(colnames(y[[i]])[which(colnames(y[[i]]) != id)],
+                values[i],
+                sep = "_")
+      } else {
+        colnames(y[[i]])[which(colnames(y[[i]])!=id)] <-
+          paste(values[i],
+                colnames(y[[i]])[which(colnames(y[[i]]) != id)],
+                sep = "_")
+      }
+
 
       y[[i]] <- as.data.frame(y[[i]])
     }
