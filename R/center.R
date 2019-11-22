add233e91a6a4974f15c3201d231503c9f29027f
Jason Tsukahara <tsuky19@gmail.com>
2019-11-22 12:24:49 -0500

    add drop parameter

--- R/center.R
+++ R/center.R
@@ -1,21 +1,25 @@
 #' Create a column of centered variables
 #'
-#' This function centers a variable around the mean. To create z-scores, specify `standardize=TRUE`
+#' This function centers a variable around the mean. To create z-scores,
+#' specify `standardize=TRUE`
 #' A new column will be created with the centered values.
 #' There is also the option to center within context.
 #' @param x dataframe
 #' @param variables c() of columns to standardize
-#' @param standardize Logical. Do you want to calculate zscores? (Default = FALSE)
-#' @keywords center
+#' @param standardize Logical. Do you want to calculate
+#' zscores? (Default = FALSE)
+#' @param drop Drop original non-centered variables
 #' @export
-#' @examples
-#' center(x, subset = c(colnames(x)), standardize = FALSE)
+#'
 
-center <- function(x, variables = c(colnames(x)), standardize = FALSE){
+center <- function(x, variables = c(colnames(x)), standardize = FALSE,
+                   drop = FALSE){
   # Perform this function for each variable specified
   for (variable in colnames(x[variables])){
     # Calculate centered scores using the scale() function
-    x <- dplyr::mutate(x, hold = scale(get(variable), center = TRUE, scale = standardize))
+    x <- dplyr::mutate(x,
+                       hold = scale(get(variable), center = TRUE,
+                                    scale = standardize))
     x$hold <- as.vector(x$hold)
     if (standardize==FALSE){
       names(x)[which(names(x)=="hold")] <- paste(variable, "_c", sep = "")
@@ -23,6 +27,11 @@ center <- function(x, variables = c(colnames(x)), standardize = FALSE){
       names(x)[which(names(x)=="hold")] <- paste(variable, "_z", sep = "")
     }
   }
+
+  if (drop == TRUE) {
+    x <- x[, -which(colnames(x) %in% variables)]
+  }
+
   return(x)
 }
 
