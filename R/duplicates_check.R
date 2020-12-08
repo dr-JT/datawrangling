5e7fa6c8543ef3c38fc845237140d6307d8ca062
Jason Tsukahara <jtsukahara3@gatech.edu>
2020-12-08 16:11:52 -0500

    add

--- R/duplicates_check.R
+++ R/duplicates_check.R
@@ -5,14 +5,14 @@
 #' @param id Subject ID variable name.
 #' @param unique Column names that are unique and should be used to
 #'     check for duplicate id's
-#' @param n Number of unique id's expected
+#' @param n Number of unique id's expected (default: 1)
 #' @param remove logical. Remove duplicate ids from data?
 #' @param save_as Folder path and file name to output the duplicate ID's
 #' @export
 
 duplicates_check <- function(x, id = "Subject",
                              unique = c("SessionDate", "SessionTime"),
-                             save_as = NULL){
+                             save_as = NULL, n = 1){
   # get duplicate ids
   duplicates <- dplyr::select(x, id, unique)
   duplicates <- dplyr::distinct(duplicates)
